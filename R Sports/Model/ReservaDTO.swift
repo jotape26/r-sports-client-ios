f4ab0d255cc99d51722233e454dba29e7fda7a31
João Leite <joao.leite@valemobi.com.br>
2019-06-10 10:25:02 -0300

    Font adjustment and new DTO

---
+++ R Sports/Model/ReservaDTO.swift
@@ -0,0 +1,13 @@
+//
+//  ReservaDTO.swift
+//  R Sports
+//
+//  Created by João Pedro Leite on 10/06/19.
+//  Copyright © 2019 João Pedro Leite. All rights reserved.
+//
+
+import Foundation
+
+class ReservaDTO {
+    
+}
