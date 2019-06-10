916552add80b5219a4c8cd839104ec953c21aa3c
João Leite <joao.leite@valemobi.com.br>
2019-06-10 10:34:43 -0300

    ReservaDTO created

--- R Sports/Model/ReservaDTO.swift
+++ R Sports/Model/ReservaDTO.swift
@@ -10,4 +10,29 @@ import Foundation
 
 class ReservaDTO {
     
+    private var quadra: QuadraDTO
+    private var data: Date
+    private var jogadores: [UserDTO]
+    
+    init(quadra: QuadraDTO, data: Date) {
+        self.quadra = quadra
+        self.data = data
+        self.jogadores = []
+    }
+    
+    func addJogador(jogador: UserDTO) {
+        jogadores.insert(jogador, at: 0)
+    }
+    
+    func getQuadra() -> QuadraDTO {
+        return quadra
+    }
+    
+    func getData() -> Date {
+        return data
+    }
+    
+    func setData(data: Date) {
+        self.data = data
+    }
 }
