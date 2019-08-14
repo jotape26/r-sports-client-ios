19ef9fb4f8be7a361a236f27f5153efb5c494a2c
João Leite <joao.leite@valemobi.com.br>
2019-08-14 17:23:33 -0300

    Changes to deal later

--- R Sports/Controllers/LoginController.swift
+++ R Sports/Controllers/LoginController.swift
@@ -31,6 +31,7 @@ class LoginController: UIViewController {
     @IBAction func btnEntrarClick(_ sender: Any) {
         self.view.startLoading()
         PhoneService.sendVerificationCode(phoneNumber: txtCelNumber.formatToPhone(), success: {
+            UserDefaults.standard.set(self.txtCelNumber.formatToPhone(), forKey: "lastUsedPhone")
             DispatchQueue.main.async {
                 UIView.animate(withDuration: 0.3, animations: {
                     self.view.stopLoading()
